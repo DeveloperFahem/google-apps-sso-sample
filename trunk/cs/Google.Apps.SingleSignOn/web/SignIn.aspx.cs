799566e6ed99bc68c1d9b2a871f6acd86fc8917a
api.alex <api.alex@bb39689b-4f33-0410-9b81-f32879492e1f>
2007-06-22 07:59:34 +0000

    Initial upload of Java/J2EE, PHP, and C# ASP.NET sample SSO code

---
+++ trunk/cs/Google.Apps.SingleSignOn/web/SignIn.aspx.cs
@@ -0,0 +1,61 @@
+// Copyright 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace Google.Apps.SingleSignOn.Web
+{
+    /// <summary>
+    /// Summary description for SignIn.
+    /// </summary>
+    public partial class SignIn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, System.EventArgs e)
+        {
+            string ssoUrl =
+                ConfigurationManager.AppSettings["Google.Apps.SingleSignOn.Url"];
+
+            Server.Transfer(ssoUrl);
+        }
+
+        #region Web Form Designer generated code
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+        }
+        #endregion
+    }
+}
